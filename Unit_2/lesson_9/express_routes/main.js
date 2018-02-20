334c4fe4f7937bd1174c970817fc1336944da40f
Jon Wexler <jwexman@gmail.com>
2018-02-20 02:01:17 -0500

    removes middleware function

--- Unit_2/lesson_9/express_routes/main.js
+++ Unit_2/lesson_9/express_routes/main.js
@@ -4,11 +4,6 @@ const port = 3000,
   express = require('express'),
   app = express();
 
-app.use((req, res, next) => {
-  console.log(`request made to: ${req.url}`);
-  next();
-});
-
 app.get('/items/:vegetable', (req, res) => {
   let veg = req.params.vegetable;
   res.send(`This is the page for ${veg}`);
