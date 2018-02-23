68365a8dec93ac20243131282221a6c4ab5dbf1f
Jon Wexler <jwexman@gmail.com>
2018-02-23 01:02:34 -0500

    lesson 6 start/finish folders

---
+++ Unit_1/lesson_6/start/serve_html/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const routeResponseMap = {
+    '/info': '<h1>Info Page</h1>',
+    '/contact': '<h1>Contact Us</h1>',
+    '/about': '<h1>Learn More About Us.</h1>',
+    '/hello': '<h1>Say hello by emailing us here</h1>',
+    '/error': '<h1>Sorry the page you are looking for is not here.</h1>'
+  },
+  port = 3000,
+  http = require('http'),
+  httpStatus = require('http-status-codes'),
+  app = http.createServer((req, res) => {
+    res.writeHead(httpStatus.OK, {
+      'Content-Type': 'text/html'
+    });
+
+    if (routeResponseMap[req.url]) {
+      res.end(routeResponseMap[req.url]);
+    } else {
+      setTimeout(() => {
+        res.end('<h1>Welcome!</h1>');
+      }, 2000);
+    }
+  });
+
+app.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
