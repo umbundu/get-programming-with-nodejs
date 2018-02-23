335fbbc26945414de3234a1c1541b48923377057
Jon Wexler <jwexman@gmail.com>
2018-02-23 00:37:20 -0500

    start and finish folders for lesson 5

---
+++ Unit_1/lesson_5/start/second_server/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const port = 3000,
+  http = require('http'),
+  httpStatus = require('http-status-codes'),
+  app = http.createServer();
+
+app.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
