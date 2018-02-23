17a674ab397845ce0e843dc18f9a1572b032784c
Jon Wexler <jwexman@gmail.com>
2018-02-23 01:18:34 -0500

    capstone 7 start and finish

---
+++ Unit_1/lesson_7_capstone/start/confetti_cuisine/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const port = 3000,
+  http = require('http'),
+  httpStatus = require('http-status-codes'),
+  router = require('./router'),
+  fs = require('fs'),
+  contentTypes = require('./content-types'),
+  utils = require('./utils');
+
+http.createServer(router.handle).listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
---
+++ Unit_1/lesson_7_capstone/start/confetti_cuisine/utils.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const fs = require('fs'),
+  httpStatus = require('http-status-codes'),
+  contentTypes = require('./content-types');
+
+module.exports = {
+  getFile: (file, res) => {
+
+  }
+};
