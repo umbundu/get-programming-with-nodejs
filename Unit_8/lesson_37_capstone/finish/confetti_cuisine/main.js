319cb076ff2fe799ba8978f6955c35f1fa22c592
Jon Wexler <jwexman@gmail.com>
2018-03-05 04:35:14 -0500

    code changes to last capstone

--- Unit_8/lesson_37_capstone/finish/confetti_cuisine/main.js
+++ Unit_8/lesson_37_capstone/finish/confetti_cuisine/main.js
@@ -1,60 +1,71 @@
 'use strict';
 
-const express = require('express'),
-layouts = require('express-ejs-layouts'),
-app = express(),
-router = require('./routes/index'),
-methodOverride = require('method-override'),
-connectFlash = require('connect-flash'),
-passport = require('passport'),
-User = require('./models/user'),
-cookieParser = require('cookie-parser'),
-expressSession = require('express-session'),
-expressValidator = require('express-validator'),
-bodyParser = require('body-parser'),
-morgan = require('morgan'),
-mongoose = require('mongoose');
+const express = require( 'express' ),
+  layouts = require( 'express-ejs-layouts' ),
+  app = express(),
+  router = require( './routes/index' ),
+
+  connectFlash = require( 'connect-flash' ),
+  passport = require( 'passport' ),
+  User = require( './models/user' ),
+  cookieParser = require( 'cookie-parser' ),
+  expressSession = require( 'express-session' ),
+  expressValidator = require( 'express-validator' ),
+  morgan = require( 'morgan' ),
+
+  bodyParser = require( 'body-parser' ),
+  mongoose = require( 'mongoose' ),
+  methodOverride = require( 'method-override' );
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/confetti_cuisine_db');
-var db = mongoose.connection;
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/confetti_cuisine_db' );
 
-const port =  process.env.PORT || 3000;
+app.set( 'port', process.env.PORT || 3000 );
+app.set( 'view engine', 'ejs' );
 
-app.set('view engine', 'ejs');
-app.use(layouts);
-app.use(express.static('public'));
-app.use(morgan('combined'));
+app.use( cookieParser( 'secretCuisine123' ) );
+app.use( expressSession( {
+  secret: 'secretCuisine123',
+  cookie: {
+    maxAge: 4000000
+  },
+  resave: false,
+  saveUninitialized: false
+} ) );
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-app.use(bodyParser.json());
+app.use( morgan( 'combined' ) );
 
-app.use(cookieParser('secretCuisine123'));
-app.use(expressSession({ secret: 'secretCuisine123', cookie: { maxAge: 4000000 }, resave: false, saveUninitialized: false}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-app.use(connectFlash());
-app.use(expressValidator());
+app.use( passport.initialize() );
+app.use( passport.session() );
+passport.use( User.createStrategy() );
+passport.serializeUser( User.serializeUser() );
+passport.deserializeUser( User.deserializeUser() );
+app.use( connectFlash() );
+app.use( expressValidator() );
 
-app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
-
-app.use((req, res, next) => {
+app.use( ( req, res, next ) => {
   res.locals.flashMessages = req.flash();
   res.locals.loggedIn = req.isAuthenticated();
   res.locals.currentUser = req.user;
   next();
-});
+} );
 
-app.use("/",router);
+app.use( methodOverride( '_method', {
+  methods: [ 'POST', 'GET' ]
+} ) );
 
-const server = app.listen(port, () => {
-  console.log("Server running at http://localhost:3000");
-});
+app.use( layouts );
+app.use( express.static( 'public' ) );
 
-const io = require('socket.io')(server),
- chatController = require('./controllers/chatController')(io);
+app.use( bodyParser.urlencoded( {
+  extended: false
+} ) );
+app.use( bodyParser.json() );
+
+app.use( '/', router );
+
+const server = app.listen( app.get( 'port' ), () => {
+    console.log( `Server running at http://localhost:${app.get('port')}` );
+  } ),
+  io = require( 'socket.io' )( server );
+require( './controllers/chatController' )( io );
